2d27d68e014738e648a544eec9c10620089b2607
RICCARDO BERTI <riccardo.berti@gmail.com>
2015-04-19 17:45:30 +0200

    testing maps

--- js/theseus.js
+++ js/theseus.js
@@ -1,9 +1,18 @@
 /*   VARIABILI  */
-var divs = ["login", "menuBtn", "benvenuti", "splash","menu", "homeTheseus", "homeClient", "homeCourier",  "aboutApp","drawer-controller-hide","drawer-controller-show", "profilo", "legend-content","loginForm","legend-position","nuovoIndirizzoForm","nuovoOrdineForm","splashScreen","bacheca"];
+var divs = ["login", "menuBtn", "benvenuti", "splash","menu", "homeTheseus", "detailTheseus", "mapTheseus",  "aboutApp","drawer-controller-hide","drawer-controller-show", "profilo", "legend-content","loginForm","legend-position","nuovoIndirizzoForm","nuovoOrdineForm","splashScreen","bacheca"];
 var mapID = "riccardante.llg16mdf";
 var mapboxAccessToken = "pk.eyJ1IjoicmljY2FyZGFudGUiLCJhIjoiLUlVekRRYyJ9.ISPJ0xA1XnwnXtE9ibSbyw";
 
 var username = "";
+var map;
+var p1;
+var myp1;
+var pCircle;
+var dest;
+var destCircle;
+var m1;
+var m2;
+var legend;
 
 
 // reverse geocoding: http://api.tiles.mapbox.com/v4/geocode/mapbox.places/{lon},{lat}.json?access_token=<your access token>
@@ -92,7 +101,22 @@ function getMyTheseus(){
 			$("#loginError").html("ERROR");
 
 			console.log("ERROR");
-            console.log(jqXHR)
+            console.log(jqXHR);
+			// FAKE COORDS
+			myTheseusItems = [{"type":"standalone", 
+					   "code":"PROTO-001", 
+					   "color":"blue",  
+					   "photo":"style/images/theseus_blue.png",
+						"posizione" : {"lat":"51.469815" , "lon" : "-0.453877", "date":"", "address":"Via Ridolfino Venuti 25, Roma, ITALIA"}
+																
+						},										
+                      {"type":"standalone", 
+					   "code":"PROTO-002", 
+					   "color":"black", 
+					   "photo":"style/images/theseus_black.png",
+					   "posizione" : {"lat":"41.794756" , "lon" : "12.249127", "date":"", "address":"Via Leonardo da Vinci, Roma, ITALIA"}
+					  }
+					 ];
         },
         success: function (msg) {
 				  $("#loginError").html("SUCCESS");
@@ -127,6 +151,16 @@ function getMyTheseus(){
 		$( this ).addClass( "done" );
 	});
 	*/
+}
+
+
+function getAddress(){
+  url = "http://api.tiles.mapbox.com/v4/geocode/mapbox.places/"+posizione.lon+","+posizione.lat+".json?access_token="+mapboxAccessToken;
+  $.getJSON( url, function( data ) {
+    posizione.address=data.features[0].place_name;
+    console.log(posizione.address);
+    //document.getElementById("leg-posizione").innerHTML = posizione.address;
+  });
 }
 
 
@@ -192,7 +226,45 @@ function showHomeTheseus(){
 
 
 function showTheseusMap(appo){
-	id_theseus = appo.data.msg;
+	  hideAll(["mapTheseus"]);
+	  id_theseus = appo.data.msg;
+	  
+	  posizione = myTheseusItems[id_theseus]['posizione'];
+	  getAddress();
+
+  //document.getElementById("legend-position").style.display = "block";
+  //document.getElementById("legend-actionbar").style.display = "block";
+
+  //document.getElementById("homeCourier").style.display = "block";
+  //document.getElementById("addtitle").innerHTML=" - " + myTheseusItems[0]['code'];
+  
+  
+  if(typeof(map) == "undefined" ){  
+    map =  L.mapbox.map('mapTheseus', mapID) .setView([posizione.lat, posizione.lon], 8);
+  }else{
+    map.setView([posizione.lat, posizione.lon], 8)
+  }
+  if(myp1!=undefined){map.removeLayer(myp1);}
+  myp1=L.marker([myPosizione.lat, myPosizione.lon],{icon:L.mapbox.marker.icon({'marker-size':'medium','marker-symbol':'star','marker-color':'#ff0000'})}).addTo(map);
+  if(pCircle!=undefined){map.removeLayer(pCircle);}  
+  pCircle = L.circle([posizione.lat, posizione.lon], 3000).addTo(map);
+  if(p1!=undefined){map.removeLayer(p1);}
+  p1 = L.marker([posizione.lat, posizione.lon], {
+    icon: L.mapbox.marker.icon({
+      'marker-size': 'medium',
+      'marker-symbol': 'star',
+      'marker-color': '#1087bf'
+    })
+  }).addTo(map);
+  
+  if(m1!=undefined){map.removeLayer(m1);}
+  if(m2!=undefined){map.removeLayer(m2);}
+  if(dest!=undefined){map.removeLayer(dest);}
+  if(destCircle!=undefined){map.removeLayer(destCircle);}
+  
+  //document.getElementById("legend-content").style.display = "none";
+  //document.getElementById("legend-position").style.display = "none";  //////////////// RIAGGIUNGI !!!!!!
+
 }
 
 
