8df73b5d4a8b62aaf124cb40e1e742cf663a02b8
RICCARDO BERTI <riccardo.berti@gmail.com>
2015-04-21 15:52:21 +0200

    cleaning code

--- js/theseus.js
+++ js/theseus.js
@@ -5,6 +5,8 @@ var mapID = "riccardante.llg16mdf";
 var mapboxAccessToken = "pk.eyJ1IjoicmljY2FyZGFudGUiLCJhIjoiLUlVekRRYyJ9.ISPJ0xA1XnwnXtE9ibSbyw";
 
 var username = "";
+var data;
+var myTheseusItems;
 var map;
 var p1;
 var myp1;
@@ -22,7 +24,6 @@ var legend;
 // la posizione va presa dal GPS
 var myPosizione = {"lat":"41.800278" , "lon" : "12.238889", "address":"impossibile ottenere la posizione"};
 
-var myTheseusItems;
 /*
 var myTheseusItems = [{"type":"standalone", 
 					   "code":"PROTO-001", 
@@ -49,6 +50,153 @@ var myTheseusItems = [{"type":"standalone",
 
 
 
+
+
+
+/****  VIEW    ****/
+function hideAll(appo){
+  for(i=0;i<divs.length;i++){
+	  $("#"+divs[i]).hide();
+  }
+  for(i=0;i<divs.length;i++){
+	  for(j=0;j<appo.length;j++){
+		if(divs[i]==appo[j]){
+			$("#"+divs[i]).show();
+		}
+	  }
+  }
+}
+
+
+
+function showLoginForm(){
+	hideAll(["login"]);
+}
+
+
+function showDashboard(){
+  username = $("#signin-username").val();
+  if(username==""){
+	  $("#loginError").html("Please insert a Username");
+		return;
+  }
+  
+  getUserData(username, showDashboardCallback);
+
+}
+
+function showDashboardCallback(){
+	console.log(data);
+	user = data['user'];
+	console.log(user);
+	myTheseusItems = data['item'];
+	
+  hideAll(["dashboard", "btn-menu", "menu"]);
+
+  appo="<ul>";
+  for(i=0;i<myTheseusItems.length ;i++){
+    appo += '<li><h1 class="communicationTitle">';
+    appo += myTheseusItems[i]["code"];
+    appo += '</h1><p>';
+    appo += '<div id="btn-theseus-'+i+'"><img src="'+myTheseusItems[i]['photo']+'" /></div>';
+    appo += '</p></li>';
+  }
+  appo += '</ul>';
+  $("#dashboard p").html(appo);
+  
+ 
+  for(j=0;j<myTheseusItems.length ;j++){
+	$("#btn-theseus-"+j).bind("click", {msg:j}, showTheseusMap);
+  }
+
+
+}
+
+/*
+function getUserCallback(){
+	$("#loginError").html("getUser callback - " + user[0].name);
+
+	//console.log(user[0].name);
+	getMyTheseus(showDashboardCallback);
+}
+
+
+function showDashboardCallback_OLD(){
+  hideAll(["dashboard", "btn-menu", "menu"]);
+
+  appo="<ul>";
+  for(i=0;i<myTheseusItems.length ;i++){
+    appo += '<li><h1 class="communicationTitle">';
+    appo += myTheseusItems[i]["code"];
+    appo += '</h1><p>';
+    appo += '<div id="btn-theseus-'+i+'"><img src="'+myTheseusItems[i]['photo']+'" /></div>';
+    appo += '</p></li>';
+  }
+  appo += '</ul>';
+  $("#dashboard p").html(appo);
+  
+ 
+  for(j=0;j<myTheseusItems.length ;j++){
+	$("#btn-theseus-"+j).bind("click", {msg:j}, showTheseusMap);
+  }
+
+}
+*/
+
+function showTheseusMap(appo){
+	  hideAll(["map", "btn-menu", "menu"]);
+	  id_theseus = appo.data.msg;
+	  
+	  posizione = myTheseusItems[id_theseus]['posizione'];
+	  getAddress();
+
+  //document.getElementById("legend-position").style.display = "block";
+  //document.getElementById("legend-actionbar").style.display = "block";
+
+  //document.getElementById("homeCourier").style.display = "block";
+  //document.getElementById("addtitle").innerHTML=" - " + myTheseusItems[0]['code'];
+  
+  
+  if(typeof(map) == "undefined" ){  
+    map =  L.mapbox.map('map', mapID).setView([posizione.lat, posizione.lon], 8);
+  }else{
+    map.setView([posizione.lat, posizione.lon], 8)
+  }
+  if(myp1!=undefined){map.removeLayer(myp1);}
+  myp1=L.marker([myPosizione.lat, myPosizione.lon],{icon:L.mapbox.marker.icon({'marker-size':'medium','marker-symbol':'star','marker-color':'#ff0000'})}).addTo(map);
+  if(pCircle!=undefined){map.removeLayer(pCircle);}  
+  pCircle = L.circle([posizione.lat, posizione.lon], 3000).addTo(map);
+  if(p1!=undefined){map.removeLayer(p1);}
+  p1 = L.marker([posizione.lat, posizione.lon], {
+    icon: L.mapbox.marker.icon({
+      'marker-size': 'medium',
+      'marker-symbol': 'star',
+      'marker-color': '#1087bf'
+    })
+  }).addTo(map);
+  
+  if(m1!=undefined){map.removeLayer(m1);}
+  if(m2!=undefined){map.removeLayer(m2);}
+  if(dest!=undefined){map.removeLayer(dest);}
+  if(destCircle!=undefined){map.removeLayer(destCircle);}
+  
+  //document.getElementById("legend-content").style.display = "none";
+  //document.getElementById("legend-position").style.display = "none";  //////////////// RIAGGIUNGI !!!!!!
+
+}
+
+function showProfile(){
+  hideAll(["profilo", "btn-menu", "menu"]);
+  appo = user[0].name + " " + user[0].surname;
+  $("#profilo p").html(appo);
+
+}
+
+
+
+
+
+
 /*   CONTROLLER   */
 function startApp(){
 	  hideAll(["splash"]);
@@ -56,6 +204,27 @@ function startApp(){
 
 
 /********* GET DATA FROM SERVER  *************/
+function getUserData(username, callback){
+	$.ajax({
+		method       : "POST",
+		data       : {username : 'username'},
+		crossDomain: true,
+		dataType   : 'json',
+        url: "http://theseus-sms.azurewebsites.net/getUserData.php"
+	})
+	.done(function (msg) {
+			data = msg;
+			callback();
+        })
+	.fail(function (jqXHR, textStatus, errorThrown) {
+			data = {"user":{"code":"1","name":"Riccardo","surname":"Berti","email":"riccardo.berti@gmail.com","image":"style\/images\/riccardo.jpg","mobile":"+39-320-3918907","password":""},"item":[{"type":"standalone","code":"PROTO-001","color":"blue","photo":"style\/images\/theseus_blue.png","posizione":{"lat":"51.469815","lon":"-0.453877","date":"","address":"Via Ridolfino Venuti 25, Roma, ITALIA"}},{"type":"standalone","code":"PROTO-1X2","color":"black","photo":"style\/images\/theseus_black.png","posizione":{"lat":"41.794756","lon":"12.249127","date":"","address":"Via Leonardo da Vinci, Roma, ITALIA"}}]};
+			callback();
+        });
+		
+	return;
+	
+}
+
 function getUser(username, callBack){
 	console.log("getUser");
 	$("#loginError").html("getUser");
@@ -233,148 +402,8 @@ function getAddress(){
 }
 
 
-/****  VIEW    ****/
-function hideAll(appo){
-	/*
-  for(i=0;i<divs.length;i++){
-	  if(divs[i]==appo[0]){
-		$("#"+divs[i]).show();
-	  }else{
-		  $("#"+divs[i]).hide();
-		}
-  }
-  */
-  for(i=0;i<divs.length;i++){
-	  $("#"+divs[i]).hide();
-  }
-  for(i=0;i<divs.length;i++){
-	  for(j=0;j<appo.length;j++){
-		if(divs[i]==appo[j]){
-			$("#"+divs[i]).show();
-		}
-	  }
-  }
-  
-}
 
 
-function showLoginForm(){
-	hideAll(["login"]);
-	//getMyTheseus();
-	
-	
-	//$("#login").show();
-	/*
-  document.getElementById("splash").style.display = "none";  
-  document.getElementById("login").style.display = "block";  
-	showDrawerController();
-	document.getElementById("loginForm").style.display = "block";
-	document.getElementById("addtitle").innerHTML=" - LOGIN";
-*/
-}
-
-function getUserCallback(){
-	$("#loginError").html("getUser callback - " + user[0].name);
-
-	//console.log(user[0].name);
-	getMyTheseus(showDashboardCallback);
-}
-
-function showDashboard(){
-
-  $("#loginError").html("WAITING ..");
-	
-  username = $("#signin-username").val();
-  if(username==""){
-	  $("#loginError").html("Please insert a Username");
-		return;
-  }
-  
-  
-  
-  getUser(username, getUserCallback);
-
-  /*
-  if(!getUser(username)){
-  	  $("#loginError").html("ERROR!");
-	  //return;
-  }  
-	*/
-	//getMyTheseus(showDashboardCallback);
-  
-  
-  
-}
-
-function showDashboardCallback(){
-  hideAll(["dashboard", "btn-menu", "menu"]);
-
-  appo="<ul>";
-  for(i=0;i<myTheseusItems.length ;i++){
-    appo += '<li><h1 class="communicationTitle">';
-    appo += myTheseusItems[i]["code"];
-    appo += '</h1><p>';
-    appo += '<div id="btn-theseus-'+i+'"><img src="'+myTheseusItems[i]['photo']+'" /></div>';
-    appo += '</p></li>';
-  }
-  appo += '</ul>';
-  $("#dashboard p").html(appo);
-  
- 
-  for(j=0;j<myTheseusItems.length ;j++){
-	$("#btn-theseus-"+j).bind("click", {msg:j}, showTheseusMap);
-  }
-
-}
-
-function showTheseusMap(appo){
-	  hideAll(["map", "btn-menu", "menu"]);
-	  id_theseus = appo.data.msg;
-	  
-	  posizione = myTheseusItems[id_theseus]['posizione'];
-	  getAddress();
-
-  //document.getElementById("legend-position").style.display = "block";
-  //document.getElementById("legend-actionbar").style.display = "block";
-
-  //document.getElementById("homeCourier").style.display = "block";
-  //document.getElementById("addtitle").innerHTML=" - " + myTheseusItems[0]['code'];
-  
-  
-  if(typeof(map) == "undefined" ){  
-    map =  L.mapbox.map('map', mapID).setView([posizione.lat, posizione.lon], 8);
-  }else{
-    map.setView([posizione.lat, posizione.lon], 8)
-  }
-  if(myp1!=undefined){map.removeLayer(myp1);}
-  myp1=L.marker([myPosizione.lat, myPosizione.lon],{icon:L.mapbox.marker.icon({'marker-size':'medium','marker-symbol':'star','marker-color':'#ff0000'})}).addTo(map);
-  if(pCircle!=undefined){map.removeLayer(pCircle);}  
-  pCircle = L.circle([posizione.lat, posizione.lon], 3000).addTo(map);
-  if(p1!=undefined){map.removeLayer(p1);}
-  p1 = L.marker([posizione.lat, posizione.lon], {
-    icon: L.mapbox.marker.icon({
-      'marker-size': 'medium',
-      'marker-symbol': 'star',
-      'marker-color': '#1087bf'
-    })
-  }).addTo(map);
-  
-  if(m1!=undefined){map.removeLayer(m1);}
-  if(m2!=undefined){map.removeLayer(m2);}
-  if(dest!=undefined){map.removeLayer(dest);}
-  if(destCircle!=undefined){map.removeLayer(destCircle);}
-  
-  //document.getElementById("legend-content").style.display = "none";
-  //document.getElementById("legend-position").style.display = "none";  //////////////// RIAGGIUNGI !!!!!!
-
-}
-
-function showProfile(){
-  hideAll(["profilo", "btn-menu", "menu"]);
-  appo = user[0].name + " " + user[0].surname;
-  $("#profilo p").html(appo);
-
-}
 
 
 
@@ -388,10 +417,8 @@ window.onload = function () {
   
   // LISTNER PARAMETRICO  $("#splash").bind("click", VARIABILE, showLoginForm);
 
-  //altezza= window.innerHeight - $("#header").css("height");
   //console.log(altezza); // NaN
   $("#map").css("height",window.innerHeight);
-  startApp();
-	$("#splash").show(1);
 
+  startApp();
 };
